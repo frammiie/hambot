83a9e0282e3850c6e83596ca354c78bc7492e8aa
frammie <fraym.phone@gmail.com>
2025-10-07 23:40:57 +0200

    feat(log): Add custom logger (config)

--- db/connection.go
+++ db/connection.go
@@ -3,9 +3,11 @@ package db
 import (
 	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"gorm.io/gorm/schema"
 )
 
@@ -13,11 +15,20 @@ var Instance *gorm.DB
 
 func Init() {
 	var err error
+	
 	Instance, err = gorm.Open(sqlite.Open(os.Getenv("DB")), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
 			SingularTable: true,
 		},
+		Logger: logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags),
+			logger.Config{
+				SlowThreshold: time.Minute,
+				IgnoreRecordNotFoundError: true,
+			},
+		),
 	})
+
 	if err != nil {
 		log.Fatal(err)
 	}
